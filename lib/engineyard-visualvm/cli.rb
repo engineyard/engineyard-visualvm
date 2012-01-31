e8498f6da17a72bafe3fecbf805e6b2b6a73677e
Nick Sieger <nick@nicksieger.com>
2012-01-31 15:36:30 -0600

    Don't explicitly require 'thor' before 'engineyard'
    Since we rely on 'engineyard' for the dependency, let
    
        require 'engineyard'
        require 'engineyard/thor'
    
    take care of loading thor. Fixes issue w/ missing dependency when
    installing engineyard version 1.4.22

--- lib/engineyard-visualvm/cli.rb
+++ lib/engineyard-visualvm/cli.rb
@@ -1,10 +1,9 @@
 #--
-# Copyright (c) 2011 Engine Yard, Inc.
+# Copyright (c) 2011, 2012 Engine Yard, Inc.
 # See the file LICENSE.txt included with the distribution for
 # software license details.
 #++
 
-require "thor"
 require "childprocess"
 require "socket"
 require "engineyard"
